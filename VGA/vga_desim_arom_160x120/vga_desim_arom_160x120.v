1dd023210896217d0de68bfee66609ab1ee9ebe1
rizki-ap <rizki.ap@gmail.com>
2026-06-17 12:39:01 +0700

    Add files via upload

---
+++ VGA/vga_desim_arom_160x120/vga_desim_arom_160x120.v
@@ -0,0 +1,66 @@
+// Copyright (c) 2020 FPGAcademy
+// Please see license at https://github.com/fpgacademy/DESim
+
+`timescale 1ns / 1ns
+`default_nettype none
+
+module vga_desim_arom_160x120 (clk,rst_n,VGA_X,VGA_Y,VGA_RGB,VGA_PLOT);
+    // Number of columns and rows in the video memory
+    parameter COLS = 160  ;
+    parameter ROWS = 120  ;
+    parameter nX = 8; // VGA x bitwidth for 640x480
+    parameter nY = 7; // VGA y bitwidth for 640x480
+
+    input wire clk;	// DE-series board 50 MHz Clock
+    input wire rst_n;		// 
+
+    output reg [nX-1:0] VGA_X;     // VGA column
+    output reg [nY-1:0] VGA_Y;     // VGA row
+    output reg [23:0] VGA_RGB;   // VGA pixel colour (24-bit color)
+    output wire VGA_PLOT;   	// VGA pixel colour (24-bit color)
+	
+	reg [23:0] color_mem [0:11];
+	initial begin
+		color_mem[0] = 24'h00003F;
+        color_mem[1] = 24'h00007F;
+        color_mem[2] = 24'h0000BF;
+        color_mem[3] = 24'h0000FF;
+        color_mem[4] = 24'h003F00;
+        color_mem[5] = 24'h007F00;
+        color_mem[6] = 24'h00BF00;
+        color_mem[7] = 24'h00FF00;
+        color_mem[8] = 24'h3F0000;
+        color_mem[9] = 24'h7F0000;
+        color_mem[10] = 24'hBF0000;
+        color_mem[11] = 24'hFF0000;
+    end
+
+	assign VGA_PLOT = 1;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (rst_n == 0) begin
+            VGA_X <= {nX{1'b0}};	// set starting x coordinate
+            VGA_Y <= {nY{1'b0}};	// set starting y coordinate
+        end
+        else begin
+		if(VGA_X < 160)	
+			VGA_X <= VGA_X + 1'b1;
+		else begin			
+			VGA_X <= {nX{1'b0}};
+			if(VGA_Y < ROWS )
+				VGA_Y <= VGA_Y + 1'b1;
+			else 
+				VGA_Y <= {nY{1'b0}};
+		end	
+	end
+    end
+	
+	always @(posedge clk or negedge rst_n) begin
+		if (rst_n == 0) 
+			VGA_RGB <= {24{1'b0}};
+		else 
+			VGA_RGB <= color_mem[(VGA_Y/10)];
+	end
+	
+
+endmodule
